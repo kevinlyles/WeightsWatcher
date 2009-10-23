fafb1da181d9a09fa599ce520fdea4b4e8eba66d
Kevin Lyles <kevinlyles@gmail.com>
2009-10-23 17:21:08 -0500

    added default variables, including temporary weights for each class

---
+++ config.lua
@@ -0,0 +1,63 @@
+defaultVars = {
+	weightsList = {
+		["ROGUE"] = {
+			["Default"] = {
+				["Attack Power"] = 100,
+			},
+		},
+		["PRIEST"] = {
+			["Default"] = {
+				["Spell Power"] = 100,
+				["Intellect"] = 65,
+			},
+			["Default2"] = {
+				["Spell Power"] = 100,
+				["Intellect"] = 50,
+			},
+		},
+		["MAGE"] = {
+			["Default"] = {
+				["Critical Strike Rating"] = 90,
+			},
+		},
+		["WARLOCK"] = {
+			["Default"] = {
+				["Haste"] = 80,
+			},
+		},
+		["HUNTER"] = {
+			["Default"] = {
+				["Agility"] = 100,
+			},
+		},
+		["WARRIOR"] = {
+			["Default"] = {
+				["Hit Rating"] = 75,
+			},
+		},
+		["DRUID"] = {
+			["Default"] = {
+				["Agility"] = 100,
+			},
+		},
+		["SHAMAN"] = {
+			["Default"] = {
+				["Agility"] = 80,
+			},
+		},
+		["DEATHKNIGHT"] = {
+			["Default"] = {
+				["Strength"] = 75,
+			},
+		},
+		["PALADIN"] = {
+			["Default"] = {
+				["Strength"] = 80,
+			},
+		},
+	},
+}
+
+defaultCharVars = {
+	activeWeights = {},
+}
