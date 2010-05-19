4e9780c989e30371a78a3443e42934753bb5db6b
Kevin Lyles <kevinlyles@gmail.com>
2010-05-18 19:59:14 -0500

    Added code to add all patterns to otherwise unused categories

--- Regexps.lua
+++ Regexps.lua
@@ -34,6 +34,27 @@ local function makePatternTables()
 			table.insert(ww_regexes[category].SingleStat, {pattern, func})
 		end
 	end
+	for _, category in pairs(patternCategories) do
+		local empty = true
+		for _ in pairs(ww_regexes[category].MultipleStat) do
+			empty = false
+			break
+		end
+		for _ in pairs(ww_regexes[category].SingleStat) do
+			empty = false
+			break
+		end
+		if empty then
+			for _, regex in ipairs(MultipleStatLines) do
+				pattern, func = unpack(regex)
+				table.insert(ww_regexes[category].MultipleStat, {pattern, func})
+			end
+			for _, regex in ipairs(SingleStatLines) do
+				pattern, func = unpack(regex)
+				table.insert(ww_regexes[category].SingleStat, {pattern, func})
+			end
+		end
+	end
 end
 
 function initializeParser()
