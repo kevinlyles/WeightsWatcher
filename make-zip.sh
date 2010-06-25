324238f6215fda8de5f53705a42b0ffd315ff62f
Kevin Lyles <kevinlyles@gmail.com>
2010-06-25 13:44:05 -0500

    Added a file with basic instructions/notes for localizers

--- make-zip.sh
+++ make-zip.sh
@@ -6,4 +6,4 @@ VERSION="`grep '^## Version: ' WeightsWatcher.toc | sed -e 's/.* //'`"
 # echo "'$VERSION'"
 # exit
 
-cd .. && 7z a -tzip -xr!'.git*' -xr!'Todo*' -xr!'*~' -xr-!'WeightsWatcher/goals' -xr-!'WeightsWatcher/future-features' -xr-!'WeightsWatcher/wowhead values' -xr-!'WeightsWatcher/make-zip.sh' -mx=9 WeightsWatcher-$VERSION.zip WeightsWatcher/
+cd .. && 7z a -tzip -xr!'.git*' -xr!'Todo*' -xr!'*~' -xr-!'WeightsWatcher/goals' -xr-!'WeightsWatcher/future-features' -xr-!'WeightsWatcher/wowhead values' -xr-!'WeightsWatcher/localization notes' -xr-!'WeightsWatcher/make-zip.sh' -mx=9 WeightsWatcher-$VERSION.zip WeightsWatcher/
