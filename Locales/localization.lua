c6897c68a9cd6f6c5f240babc7fe73b17b9d71fd
Kevin Lyles <kevinlyles@gmail.com>
2011-01-23 15:28:07 -0600

    Added a once-per-session message about non-localized strings

--- Locales/localization.lua
+++ Locales/localization.lua
@@ -1,6 +1,8 @@
 local metatable = {
 	__index = function(tbl, key)
 -- 		error("Localized string not found: " .. tostring(key))
+		print("WeightsWatcher: Warning: localized string not found: \"" .. tostring(key) .. "\"")
+		tbl[key] = key
 		return key
 	end
 }
