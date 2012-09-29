582edcb661f47c769045b483d896aa0a5ed6934c
Kevin Lyles <kevinlyles@gmail.com>
2012-09-28 21:03:01 -0500

    Removed an unused file

--- make-zip.sh
+++ make-zip.sh
@@ -6,4 +6,4 @@ VERSION="`grep '^## Version: ' WeightsWatcher.toc | sed -e 's/.* //'`"
 # echo "'$VERSION'"
 # exit
 
-cd .. && 7z a -tzip -xr!'.git*' -xr!'Todo*' -xr!'*~' -xr-!'WeightsWatcher/goals' -xr-!'WeightsWatcher/future-features' -xr-!'WeightsWatcher/wowhead values' -xr-!'WeightsWatcher/localization notes' -xr-!'WeightsWatcher/*.sh' -xr-!'WeightsWatcher/unknown-gems' -xr-!'WeightsWatcher/gem-ids*' -xr-!'WeightsWatcher/create-*' -mx=9 WeightsWatcher-$VERSION.zip WeightsWatcher/
+cd .. && 7z a -tzip -xr!'.git*' -xr!'Todo*' -xr!'*~' -xr-!'WeightsWatcher/goals' -xr-!'WeightsWatcher/future-features' -xr-!'WeightsWatcher/wowhead values' -xr-!'WeightsWatcher/localization notes' -xr-!'WeightsWatcher/*.sh' -xr-!'WeightsWatcher/gem-ids*' -xr-!'WeightsWatcher/create-*' -mx=9 WeightsWatcher-$VERSION.zip WeightsWatcher/
